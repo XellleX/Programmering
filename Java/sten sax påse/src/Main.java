efb3e7be05ada3aebdb4bb17b3924e3ba8277653
XellleX <elias.axelsson.te12d@gmail.com>
2014-09-01 13:36:50 +0200

    Java

--- Java/sten sax påse/src/Main.java
+++ Java/sten sax påse/src/Main.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main 
 {
 	public static void main(String[] args) 
 	{
+		System.out.println("You play to 3");
+		
 		Scanner scan = new Scanner(System.in);
 		
 		String[] choices = {"sten", "sax", "påse"};
 		
-		System.out.println("You play to 3");
+		int randomNumber;
+		randomNumber = (int) Math.random() * 3;
+		
 		
 		String playerChoice = scan.nextLine();
 
+		
+	}
+	
+	public static void calculating(int a)
+	{
 		if(playerChoice.equalsIgnoreCase(choices[0]))
 		{
 			System.out.println("Working");
@@ -21,7 +31,6 @@ public class Main
 		{
 			System.out.println("Not working");
 		}
-		
 	}
 
 }
